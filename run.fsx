5d997b5d17bb6bf39e80dd9c66e9db8631c2724d
michael nikiforos <michael.nikiforos@gmail.com>
2018-09-18 12:29:13 -0500

    forgot couple stuff

--- run.fsx
+++ run.fsx
@@ -1,2 +1,35 @@
-let Run(inputMessage: string, log: TraceWriter) =
-    log.Info(sprintf "F# Queue trigger function processed: '%s'" inputMessage)
+open System
+open System.IO
+open System.Net
+open System.Net.Http.Headers
+open System.Collections.Generic
+open Newtonsoft.Json
+
+let generateIds input =
+    let dictionary = new Dictionary<int, int>()
+    JsonConvert.DeserializeObject<List<int>> input
+    |> Seq.iter (fun x ->
+        let rec hasKey key =
+            if(dictionary.ContainsKey(key)) then
+                hasKey(key+1)
+            else dictionary.[key] <- x
+        hasKey((new System.Random(x)).Next())
+    )
+    dictionary
+
+let createResponse data =
+    let response = new HttpResponseMessage()
+    response.Content <- new StringContent(data)
+    response.StatusCode <- HttpStatusCode.OK
+    response.Content.Headers.ContentType <- MediaTypeHeaderValue("application/json")
+    response
+
+let Run (req: HttpRequestMessage) =
+    req.GetQueryNameValuePairs()
+    |> Seq.find(fun x -> x.Key.ToLowerInvariant() = "data")
+    |> fun x -> x.Value
+    |> generateIds
+    |> JsonConvert.SerializeObject
+    |> createResponse
+
+// no it isn't
